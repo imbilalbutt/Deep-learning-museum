e8b555edc5c12aa79ffb8ffa2b108b6a60e9949f
Bilal Ahmad Butt <bilal.ahmad125@yahoo.com>
2024-06-15 21:12:01 +0200

    a1 working -- linear regression also working

--- Advance_Deep_Learning/a1.py
+++ Advance_Deep_Learning/a1.py
@@ -6,7 +6,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import pairwise_distances
 from torchvision import transforms
 from PIL import Image
-# import matplotlib.pyplot as plt
 
 
 def perturb_image_v2(img, perturbation, segments):
@@ -30,32 +29,13 @@ def preprocess_and_predict(image, model, perturbation_times):
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
-    preturbed_preprocess = transforms.Compose([
+    perturbed_preprocess = transforms.Compose([
         transforms.ToPILImage(),
         transforms.Resize(299),
         transforms.CenterCrop(299),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-
-    input_batch = preprocess(image)
-    input_tensor = input_batch.unsqueeze(0)
-
-    # original_image_tensor_copy = copy.deepcopy(image)
-    # batches = np.stack((original_image_tensor, original_image_tensor_copy), axis=0)
-    # batches_tensor = torch.from_numpy(batches).float()
-
-    # image_copy = copy.deepcopy(image)
-    # batches = np.stack((image_copy, image))
-    # batches_tensor = preprocess(batches)
-
-    # np.random.seed(222)
-    # original_prediction = model(batches_tensor)
-    # # original_prediction = model(input_batch)
-
-    # with torch.no_grad():
-    #     np.random.seed(222)
-    #     output = model(input_batch)
 
     superpixels_or_segments = quickshift(image, kernel_size=3, ratio=0.4)
 
@@ -65,27 +45,16 @@ def preprocess_and_predict(image, model, perturbation_times):
     perturbations = np.random.binomial(1, 0.5, size=(perturbation_times, num_superpixels))
 
     perturbed_images = []
-    predictions = []
-
-
-
-    # for i in range(perturbation_times):
-    #     perturbed_image = perturb_image_v2(image, perturbations[i], superpixels_or_segments)
-    #     perturbed_images.append(perturbed_image)
-    #
-    # perturbed_images_tensor = np.stack(perturbed_images)
-    # # input_batch = perturbed_images_tensor.unsqueeze(0)
-    #
-    # perturbed_image_tensor = pertubed_preprocess(perturbed_images_tensor)
-    # np.random.seed(222)
-    # prediction = model(perturbed_image_tensor)
-
     for i in range(perturbation_times):
         perturbed_image = perturb_image_v2(np.array(image), perturbations[i], superpixels_or_segments)
-        perturbed_image_tensor = preturbed_preprocess(perturbed_image)
+        perturbed_image_tensor = perturbed_preprocess(perturbed_image)
         perturbed_images.append(perturbed_image_tensor.unsqueeze(0))
 
     perturbed_images_tensor = torch.cat(perturbed_images)
+
+    input_preprocess_image = preprocess(image)
+    input_tensor = input_preprocess_image.unsqueeze(0)
+
     batch = torch.from_numpy(np.concatenate((input_tensor, perturbed_images_tensor), axis=0))
 
     with torch.no_grad():
@@ -93,7 +62,12 @@ def preprocess_and_predict(image, model, perturbation_times):
 
     # top_pred_classes = np.argsort(predictions[0])[-5:][::-1] # Save ids of top 5 classes
 
-    # predictions.append(prediction.detach().numpy())
+    # Tensor of shape 1000, with confidence scores over ImageNet's 1000 classes
+    # print(predictions[0])
+
+    # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
+    probabilities = torch.nn.functional.softmax(predictions[0], dim=0)
+    print(probabilities)
 
     return predictions, perturbations, num_superpixels
 
@@ -103,16 +77,22 @@ def lime_explanation(image, model, perturbation_times=10):
 
     original_image = np.ones(num_superpixels)[np.newaxis, :]
 
-    distances = pairwise_distances(perturbations, original_image, metric='euclidean').ravel().reshape(1,-1)
+    distances = pairwise_distances(perturbations, original_image, metric='euclidean').ravel() # .reshape(1,-1)
 
-    weights = np.exp(-distances / np.std(distances)).reshape(1,-1)
+    weights = np.exp(-distances / np.std(distances)) #.reshape(1,-1)
+    # weights = np.transpose(np.exp(-distances / np.std(distances)) #.reshape(1,-1)
+
+    top_pred_classes = predictions[0].argsort()[-5:][::1]
+    class_to_explain = top_pred_classes[0]
 
     interpretable_model = LinearRegression()
-    interpretable_model.fit(perturbations, np.squeeze(predictions, axis=1), sample_weight=weights)
+    # interpretable_model.fit(perturbations, np.squeeze(predictions, axis=1), sample_weight=weights)
+    # interpretable_model.fit(perturbations, predictions[1:].detach().numpy(), sample_weight=weights)
+    interpretable_model.fit(X=perturbations, y=predictions[1:, class_to_explain], sample_weight=weights)
 
     explanation = interpretable_model.coef_
 
-    return explanation.reshape(image.shape)
+    return explanation
 
 
 if __name__ == "__main__":
@@ -128,8 +108,22 @@ if __name__ == "__main__":
     # Load Inception v3 neural network model
     model = torch.hub.load('pytorch/vision:v0.10.0', 'inception_v3', pretrained=True)
 
-    # model.aux_logits = False
+    model.aux_logits = False
     # model.drop_last = True
     explanation = lime_explanation(img1, model, perturbation_times=10)
 
     print("Explanation:", explanation)
+
+
+def get_prediction_labels(probabilities):
+    file_url = "https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt"
+    # Download ImageNet labels
+
+    # Read the categories
+    with open("..//imagenet_classes.txt", "r") as f:
+        categories = [s.strip() for s in f.readlines()]
+
+    # Show top categories per image
+    top5_prob, top5_catid = torch.topk(probabilities, 5)
+    for i in range(top5_prob.size(0)):
+        print(categories[top5_catid[i]], top5_prob[i].item())
